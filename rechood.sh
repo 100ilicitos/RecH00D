7920dc77de67b5b07134e030896b3f2cbdb92724
David Falconi (🅷00🅳) <112734343+100ilicitos@users.noreply.github.com>
2024-04-25 22:16:04 -0300

    Update rechood.sh

--- rechood.sh
+++ rechood.sh
@@ -11,18 +11,26 @@ if [ $# -eq 0 ]
     echo "Usage: ./rechood.sh domain.com"
     echo ""
 else
-    echo "## RECH00D.SH STARTING ##"
-    echo "You can go for lunch or dinner, it will take a while! :P"
+    echo "### RECH00D.SH STARTING ###"
+    echo "> You can go for lunch or dinner, it will take a while! :P"
     assetfinder -subs-only $DOMAIN  > domains_asset
+    echo "[+] assetfinder finish! Next..."
     amass enum -d $DOMAIN -passive  > domains_amass_passive
+    echo "[+] amass recon finish! Next..."
     amass enum -d $DOMAIN -active -brute -w /usr/share/seclists/Discovery/DNS/deepmagic.com-prefixes-top50000.txt  > domains_amass_active
+    echo "[+] amass brute force finish! Next..."
     subfinder -d $DOMAIN  > domains_sub
+    echo "[+] subfinder recon finish! Next..."
     findomain -t $DOMAIN  > domains_find
+    echo "[+] findomain recon finish! Next..."
     echo "$DOMAIN"  haktrails subdomains  > domains_hak
     cat domains*  > dominioTotal.txt
     amass enum -nf dominioTotal.txt -passive  > domains_amass_passive2
     amass enum -nf dominioTotal.txt -active -brute -w /usr/share/seclists/Discovery/DNS/deepmagic.com-prefixes-top50000.txt > domains_amass_active_2
+    echo "[+] amass brute force 2 finish! Next..."
     cat domains_amass_passive2 domains_amass_active_2  > domainsTotal2
     cat domainsTotal2  haktrails subdomains  > domains_hak2
     cat dominioTotal.txt domainsTotal2 domains_hak2  > webTotal
+    echo "> Finish!"
+
 fi
